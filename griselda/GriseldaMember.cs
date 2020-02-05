724ae92ee1f75e4921cad9d612d32a31a9535609
Nathan J. Morris | 莫南森 <59628824+pownathan77@users.noreply.github.com>
2020-02-04 19:59:33 -0500

    a
    a

---
+++ griselda/GriseldaMember.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace griselda
+{
+    class GriseldaMember: Rapper
+    {
+        string _name;
+        public GriseldaMember(string name)
+        {
+            _name = name;
+            haveBars = true;
+            canSing = false;
+        }
+    }
+}
---
+++ griselda/Rapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace griselda
+{
+    class Rapper
+    {
+        public Boolean haveBars;
+        public Boolean canSing;
+        public string Rap()
+        {
+            return "";
+        }
+    }
+}
